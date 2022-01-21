b82d809a5cd27c8ecb2e18613f2529ed136059d6
yxz <550507808@qq.com>
2022-01-21 22:32:45 +0800

    tmp

--- cmd/portmap/server/main.go
+++ cmd/portmap/server/main.go
@@ -6,50 +6,27 @@ import (
 	"fmt"
 	"github.com/forwardalex/Ytool/log"
 	"github.com/forwardalex/Ytool/tool"
-	"github.com/forwardalex/ysocks/conf"
 	redisDB "github.com/forwardalex/ysocks/dao"
 	"github.com/forwardalex/ysocks/tcp"
+	"github.com/tal-tech/go-zero/core/conf"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
 
-var defaultProperties = &conf.ServerProperties{
-	Bind:           "0.0.0.0",
-	Port:           6399,
-	AppendOnly:     false,
-	AppendFilename: "",
-	MaxClients:     1000,
-}
 
 func main() {
-	var filepath string
 	closeChan := make(chan struct{})
-	flag.StringVar(&filepath, "filepath", "./conf/config.yaml", "configfilepath")
-	flag.Parse()
-	err := conf.Init(filepath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 	tool.Init()
 	log.Init("Dev")
 	ctx := context.Background()
 	log.Info(ctx,"sever start ","ok ")
-	if err := redisDB.Init(conf.Conf.RedisConfig); err != nil {
-		fmt.Printf("init redis failed,err:=%v\n", err)
-		return
-	}
-	if conf.Conf.MachineID == 1 {
-		go tcp.ClientMain(ctx,conf.Conf.ServerPortConfg.LocaServerPort, conf.Conf.ServerPortConfg.Host)
-	}
-	if conf.Conf.MachineID == 2 {
-		go tcp.ServerMain(ctx,conf.Conf.ServerPortConfg.EscServerPort)
-		go tcp.ListenAndServeWithSignal(&tcp.Config{
-			Address: fmt.Sprintf("%s:%d", conf.Properties.Bind, conf.Properties.Port),
-		}, tcp.MakeEchoHandler(), closeChan)
-	}
+
+	go tcp.ClientMain(ctx,conf.Conf.ServerPortConfg.LocaServerPort, conf.Conf.ServerPortConfg.Host)
+
+
+
 	quit := make(chan os.Signal, 1) // 创建一个接收信号的通道
 	// kill 默认会发送 syscall.SIGTERM 信号
 	// kill -2 发送 syscall.SIGINT 信号，我们常用的Ctrl+C就是触发系统SIGINT信号
